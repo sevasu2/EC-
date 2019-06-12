7529bc827da889377c2769c207cd733939994617
Sevasu2 <babagame7@gmail.com>
2019-06-13 08:47:45 +0900

    :recycle:金額表示のメソッドの修正

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,23 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+      redirect_to @product, notice: "商品「#{@product.name}」を登録しました。"
+    else
+      flash[:error] = '作成に失敗'
+      render :new
+  end
+
+  private
+
+  def task_params
+    params.require(:product).permit(:name, :description, :price, :unit)
+  end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,14 +8,4 @@ validates :description, presence: true
 validates :price, presence: true
 validates :unit, presence: true
 
-# 修正必須（helperに書く）
-def display_price
-    case unit
-      when 'yen'
-        "#{price.to_s(:delimited)} 円"
-      when 'usd'
-        "$ #{price.to_s(:delimited)}"
-    end
-end
-
 end
