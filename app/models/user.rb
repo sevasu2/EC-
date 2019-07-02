77eda6e13c649adfd8a61e549c32cc3d1352097d
Sevasu2 <babagame7@gmail.com>
2019-07-02 18:03:54 +0900

    :sparkles:決算機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,18 @@ class User < ApplicationRecord
 #  ** Association **
 #----------------------------------------
   has_one :basket, dependent: :destroy
+  has_one :purchase_record, dependent: :destroy
 
   # ユーザーのカートの処理（カートがない場合にカートが作られる）
   def prepare_basket
     basket || create_basket
   end
+
+  def prepare_purchase_record
+     purchase_record || create_purchase_record
+  end
+
+  purchase_record = prepare_purchase_record
+  ids = product_ids.map { |id| { product_id: id } }
+  purchase_record.purchase_record_products.create!(ids)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'products#index'
   resource :basket, only: [:show]
+  resource :charge, only: [:create]
   resources :products, only: [:index, :new, :create, :show] do
     scope module: :products do
       resources :add_to_baskets, only: [:create]
