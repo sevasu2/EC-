f65f48c1cc5fb30ae08d564fb7310d50bfb85bc3
sevasu2 <babagame7@gmail.com>
2019-07-02 05:32:06 +0000

    カート

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,4 +1,6 @@
 class Basket < ApplicationRecord
+  # 米ドルのレート設定
+  USD_RATE = 110.freeze
 
 #----------------------------------------
 #  ** Association **
@@ -6,4 +8,20 @@ class Basket < ApplicationRecord
     belongs_to :user
     has_many :basket_products, dependent: :destroy
     has_many :products, through: :basket_products
+
+    def total_price
+      total_jpy_price + total_usd_price
+    end
+
+private
+  def total_jpy_price
+    jp_products = products.where(unit: 'yen')
+    jp_products.sum(:price)
+  end
+
+  def total_usd_price
+    us_products = products.where(unit: 'usd')
+    us_products.sum(:price) * USD_RATE
+  end
+
 end
